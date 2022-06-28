606982f045b501af27df881abb0b9c0faedb4782
Jessica Ho <jessicadinhho@gmail.com>
2022-06-27 17:03:03 -0700

    commit tree 24fa3103e356de50ad2ea19448951638d4f01b4f

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+  let freeSample, thirtyBucks, tooLong
+  if (someValue <= 400) {
+    freeSample = "This one is on me!"
+    return freeSample
+  }
+  else if (someValue > 400 && someValue <=2000){
+    return 'That will be twenty bucks.'
+  }
+  else if (someValue > 2000 && someValue < 2500) {
+    thirtyBucks = "I will gladly take your thirty bucks."
+    return thirtyBucks}
+
+  else if (someValue > 2500) {
+    tooLong = "No can do."
+    return tooLong};
+  }
   // Write your code here!
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  let NYC = "NYC";
+  return (city === NYC ? "Ok, sounds good." : "No go." )
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
+  let reply;
+  switch (tip) {
+    case 'generous':
+      reply = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      reply = 'Thank you.';
+      break;
+    default:
+      reply = 'Bye.';
+      break;}
+  return reply;
+  }
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
